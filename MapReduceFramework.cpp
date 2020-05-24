1b7f1db1bd84c7975d8702e8f885e226712955ce
RotemShavit <50513126+RotemShavit@users.noreply.github.com>
2020-05-24 21:28:17 +0300

    Add files via upload

--- MapReduceFramework.cpp
+++ MapReduceFramework.cpp
@@ -1,5 +1,6 @@
 #include <pthread.h>
 #include "MapReduceFramework.h"
+#include "MapReduceClient.h"
 #include <iostream>
 #include <atomic>
 
@@ -11,7 +12,9 @@ pthread_mutex_t* map_mutexes;
 
 int numOfThreads;
 
-vector<vector<IntermediatePair>> all_inter_vec;
+vector<IntermediatePair>* all_inter_vec;
+
+IntermediateMap interMap;
 
 atomic<int> atm(0);
 atomic<int> numOfProcessed(0);
@@ -21,10 +24,11 @@ typedef struct Context
     int tid;
     const InputVec& inputVec;
     pthread_mutex_t* mutex;
+    const MapReduceClient& client;
     // constructor
-    Context(int tid, const InputVec& inputVec, pthread_mutex_t* mutex):
+    Context(int tid, const InputVec& inputVec, pthread_mutex_t* mutex, const MapReduceClient& client):
     //
-    tid(tid), inputVec(inputVec), mutex(mutex){}
+    tid(tid), inputVec(inputVec), mutex(mutex), client(client){}
     //
 }Context;
 
@@ -44,6 +48,15 @@ void mutexUnLock(pthread_mutex_t* mutex)
     }
 }
 
+void emit2 (K2* key, V2* value, void* context)
+{
+    auto thread_context = (Context*) context;
+    IntermediatePair intermediatePair =  IntermediatePair(key, value);
+    mutexLock(thread_context->mutex);
+    all_inter_vec[thread_context->tid].push_back(intermediatePair);
+    mutexUnLock(thread_context->mutex);
+}
+
 void mapThreadFunc(Context* thread_context)
 {
     InputPair inputPair;
@@ -52,18 +65,36 @@ void mapThreadFunc(Context* thread_context)
     while(atm < thread_context->inputVec.size())
     {
         old_val = atm.fetch_add(1);
-        inputPair = thread_context->inputVec.at(old_val);
-        void* context = &inputPair;
-        emit2(intermediatePair.first, intermediatePair.second, context);
-        mutexLock(thread_context->mutex);
-        all_inter_vec.at(thread_context->tid).push_back(intermediatePair);
-        mutexUnLock(thread_context->mutex);
+        if(old_val < thread_context->inputVec.size())
+        {
+            inputPair = thread_context->inputVec.at(old_val);
+            thread_context->client.map(inputPair.first, inputPair.second, thread_context);
+        }
     }
 }
 
-void shuffleThreadFunc()
+void shuffleThreadFunc(Context* thread_context)
 {
-
+    // while statement for map phase finished (barrier and atomic variable)
+    for(int i = 0; i < numOfThreads - 1; i++)
+    {
+        while(!all_inter_vec[i].empty())
+        {
+            mutexLock(&map_mutexes[i]);
+            // if key in interMap, add to the specified key vector
+            // else add a new key and a new value for it
+            auto search = interMap.find(all_inter_vec[i].at(0).first);
+            if(search != interMap.end())
+            {
+                // insert the value to the key vector
+            }
+            else
+            {
+                // insert a new key map
+            }
+            mutexUnLock(&map_mutexes[i]);
+        }
+    }
 }
 
 void* threadFunc(void* context)
@@ -76,7 +107,7 @@ void* threadFunc(void* context)
     else
     {
         //run shuffle
-        shuffleThreadFunc();
+        shuffleThreadFunc(thread_context);
     }
     return nullptr;
 }
@@ -89,10 +120,11 @@ JobHandle startMapReduceJob(const MapReduceClient& client,
     numOfThreads = multiThreadLevel;
     threads = (pthread_t*)malloc(sizeof(pthread_t) * multiThreadLevel);
     map_mutexes = (pthread_mutex_t*)malloc(sizeof(pthread_mutex_t) * multiThreadLevel);
+    all_inter_vec = (vector<IntermediatePair>*)malloc(sizeof(vector<IntermediatePair>*) * (multiThreadLevel - 1));
     for(int i = 0; i < multiThreadLevel; i++)
     {
         pthread_mutex_init(&map_mutexes[i], nullptr);
-        auto* context = new Context(threads[i], inputVec, &map_mutexes[i]);
+        auto* context = new Context(threads[i], inputVec, &map_mutexes[i], client);
         pthread_create(&threads[i], nullptr, threadFunc, context);
     }
 }
